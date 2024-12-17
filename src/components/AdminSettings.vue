f88ff11628251653715c461a0fa37569ee3c8b2f
Edward Ly <contact@edward.ly>
2024-12-17 10:48:32 -0800

    feat(AdminSettings): add empty content and button components
    Signed-off-by: Edward Ly <contact@edward.ly>

--- src/components/AdminSettings.vue
+++ src/components/AdminSettings.vue
@@ -9,20 +9,60 @@
 		</div>
 		<NcSettingsSection
 			:name="t('scim_client', 'Registered Servers')"
-			:aria-label="t('scim_client', 'Registered Servers')" />
+			:aria-label="t('scim_client', 'Registered Servers')">
+			<NcEmptyContent
+				:name="t('scim_client', 'No servers configured')"
+				:description="t('scim_client', 'Register a new server below and fill in the required details')">
+				<template #icon>
+					<FormatListBullet :size="20" />
+				</template>
+			</NcEmptyContent>
+			<NcButton
+				type="primary"
+				style="margin: 20px 0;"
+				@click="showRegister">
+				{{ t('app_api', 'Register') }}
+				<template #icon>
+					<Plus v-if="!registering" :size="20" />
+					<NcLoadingIcon v-else />
+				</template>
+			</NcButton>
+		</NcSettingsSection>
 	</div>
 </template>
 
 <script>
+import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
+import NcEmptyContent from '@nextcloud/vue/dist/Components/NcEmptyContent.js'
+import NcLoadingIcon from '@nextcloud/vue/dist/Components/NcLoadingIcon.js'
 import NcSettingsSection from '@nextcloud/vue/dist/Components/NcSettingsSection.js'
+
+import FormatListBullet from 'vue-material-design-icons/FormatListBulleted.vue'
+import Plus from 'vue-material-design-icons/Plus.vue'
 
 import ScimClientIcon from './icons/ScimClientIcon.vue'
 
 export default {
 	name: 'AdminSettings',
 	components: {
+		FormatListBullet,
+		NcButton,
+		NcEmptyContent,
+		NcLoadingIcon,
 		NcSettingsSection,
+		Plus,
 		ScimClientIcon,
+	},
+	data() {
+		return {
+			registering: false,
+			showRegisterModal: false,
+		}
+	},
+	methods: {
+		showRegister() {
+			this.showRegisterModal = true
+		},
 	},
 }
 </script>
