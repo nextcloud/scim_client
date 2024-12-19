2ef8dc0a9288f657f7468d888638e4c09242690f
Edward Ly <contact@edward.ly>
2024-12-19 11:25:24 -0800

    build: update vite config and npm scripts by mode
    Signed-off-by: Edward Ly <contact@edward.ly>

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,17 @@
-import { createAppConfig } from "@nextcloud/vite-config";
-import { join, resolve } from "path";
+import { createAppConfig } from '@nextcloud/vite-config'
+import { join, resolve } from 'path'
+
+const isProduction = process.env.NODE_ENV === 'production'
 
 export default createAppConfig(
-  {
-    "admin-settings": resolve(join("src", "admin-settings.js")),
-  },
-  {
-    createEmptyCSSEntryPoints: true,
-    extractLicenseInformation: true,
-    thirdPartyLicense: false,
-    inlineCSS: true,
-  }
-);
+	{
+		'admin-settings': resolve(join('src', 'admin-settings.js')),
+	},
+	{
+		createEmptyCSSEntryPoints: true,
+		extractLicenseInformation: true,
+		thirdPartyLicense: false,
+		inlineCSS: { relativeCSSInjection: true },
+		minify: isProduction,
+	}
+)
