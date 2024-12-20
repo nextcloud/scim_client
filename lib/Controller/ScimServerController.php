243f2223102e4a453c0d34531b1e4d7b231bb176
Edward Ly <contact@edward.ly>
2024-12-20 10:44:01 -0800

    feat: rename initial controller class, add routes
    Signed-off-by: Edward Ly <contact@edward.ly>

---
+++ lib/Controller/ScimServerController.php
@@ -0,0 +1,115 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\ScimClient\Controller;
+
+use OCA\ScimClient\AppInfo\Application;
+use OCA\ScimClient\Db\ScimServer;
+use OCA\ScimClient\Service\ScimApiService;
+use OCA\ScimClient\Service\ScimServerService;
+use OCP\AppFramework\ApiController;
+use OCP\AppFramework\Http\Attribute\FrontpageRoute;
+use OCP\AppFramework\Http\Attribute\PasswordConfirmationRequired;
+use OCP\AppFramework\Http\JSONResponse;
+use OCP\AppFramework\Http\Response;
+use OCP\IRequest;
+use OCP\Security\ICrypto;
+
+class ScimServerController extends ApiController {
+
+	public function __construct(
+		IRequest $request,
+		private readonly ScimApiService $scimApiService,
+		private readonly ScimServerService $scimServerService,
+		private readonly ICrypto $crypto,
+	) {
+		parent::__construct(Application::APP_ID, $request);
+	}
+
+	#[FrontpageRoute(verb: 'GET', url: '/servers')]
+	public function getAllScimServers(): Response {
+		return new JSONResponse($this->scimServerService->getRegisteredScimServers());
+	}
+
+	#[PasswordConfirmationRequired]
+	#[FrontpageRoute(verb: 'POST', url: '/servers')]
+	public function registerScimServer(array $params): Response {
+		$server = $this->scimServerService->registerScimServer($params);
+
+		return new JSONResponse([
+			'success' => (bool)$server,
+			'server' => $server,
+		]);
+	}
+
+	#[PasswordConfirmationRequired]
+	#[FrontpageRoute(verb: 'PUT', url: '/servers/{id}')]
+	public function updateScimServer(int $id, array $params): Response {
+		// Restore original API key if dummy secret is provided
+		$apiKey = $params['api_key'] ?? null;
+		if ($apiKey === Application::DUMMY_SECRET) {
+			$server = $this->scimServerService->getScimServer($id);
+			$params['api_key'] = $server->getApiKey() ?? '';
+		} elseif (!empty($apiKey)) {
+			// New password provided, encrypt it
+			$params['api_key'] = $this->crypto->encrypt($apiKey);
+		}
+
+		// Update the server config
+		$params['id'] = $id;
+		$updatedServer = new ScimServer($params);
+		$updatedServer = $this->scimServerService->updateScimServer($updatedServer);
+
+		// Check if update was successful before proceeding
+		if (!$updatedServer) {
+			return new JSONResponse([
+				'success' => false,
+				'server' => null,
+			]);
+		}
+
+		// Mask API key with dummy secret if set
+		if (!empty($updatedServer->getApiKey() ?? null)) {
+			$updatedServer->setApiKey(Application::DUMMY_SECRET);
+		}
+
+		return new JSONResponse([
+			'success' => true,
+			'server' => $updatedServer,
+		]);
+	}
+
+	#[PasswordConfirmationRequired]
+	#[FrontpageRoute(verb: 'DELETE', url: '/servers/{id}')]
+	public function unregisterScimServer(int $id): Response {
+		$server = $this->scimServerService->getScimServer($id);
+		$server = $this->scimServerService->unregisterScimServer($server);
+
+		// Do not show API key in response
+		if (isset($server)) {
+			$server = $server->jsonSerialize();
+			unset($server['api_key']);
+		}
+
+		return new JSONResponse([
+			'success' => (bool)$server,
+			'server' => $server,
+		]);
+	}
+
+	#[FrontpageRoute(verb: 'POST', url: '/servers/{id}/verify')]
+	public function verifyExistingScimServer(int $id): Response {
+		$server = $this->scimServerService->getScimServer($id)->jsonSerialize();
+		if (!empty($server['api_key'])) {
+			$server['api_key'] = $this->crypto->decrypt($server['api_key']);
+		}
+
+		return new JSONResponse($this->scimApiService->verifyScimServer($server));
+	}
+
+	#[FrontpageRoute(verb: 'POST', url: '/servers/verify')]
+	public function verifyNewScimServer(array $server): Response {
+		return new JSONResponse($this->scimApiService->verifyScimServer($server));
+	}
+}
