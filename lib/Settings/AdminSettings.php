796a30c8d164cee81d73e498cc84be77bbad72c6
Edward Ly <contact@edward.ly>
2024-12-20 10:55:04 -0800

    feat(AdminSettings): add current server list to initial state
    Signed-off-by: Edward Ly <contact@edward.ly>

--- lib/Settings/AdminSettings.php
+++ lib/Settings/AdminSettings.php
@@ -5,17 +5,25 @@ declare(strict_types=1);
 namespace OCA\ScimClient\Settings;
 
 use OCA\ScimClient\AppInfo\Application;
+use OCA\ScimClient\Service\ScimServerService;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
 use OCP\Settings\ISettings;
 use OCP\Util;
 
 class AdminSettings implements ISettings {
 
-	public function __construct() {
+	public function __construct(
+		private readonly ScimServerService $scimServerService,
+		private readonly IInitialState $initialStateService,
+	) {
 	}
 
 	public function getForm() {
 		Util::addScript(Application::APP_ID, Application::APP_ID . '-admin-settings');
+
+		$serverList = $this->scimServerService->getRegisteredScimServers();
+		$this->initialStateService->provideInitialState('admin-server-list', $serverList);
 
 		return new TemplateResponse(Application::APP_ID, 'admin-settings');
 	}
