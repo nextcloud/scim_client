8a2c07b6c1eeaf6e086f5cab1581f5de25065732
Edward Ly <contact@edward.ly>
2024-12-20 10:42:16 -0800

    feat: add initial services for SCIM API network requests
    Signed-off-by: Edward Ly <contact@edward.ly>

---
+++ lib/Service/NetworkService.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\ScimClient\Service;
+
+use Exception;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use OCA\ScimClient\AppInfo\Application;
+use OCP\Http\Client\IClient;
+use OCP\Http\Client\IClientService;
+use OCP\IConfig;
+use OCP\IL10N;
+use OCP\PreConditionNotMetException;
+use Psr\Log\LoggerInterface;
+use Throwable;
+
+/**
+ * Service to make network requests
+ */
+class NetworkService {
+
+	public const ALLOWED_VERBS = ['get', 'post', 'put', 'patch', 'delete'];
+
+	private IClient $client;
+
+	public function __construct(
+		IClientService $clientService,
+		private IConfig $config,
+		private LoggerInterface $logger,
+		private IL10N $l10n,
+	) {
+		$this->client = $clientService->newClient();
+	}
+
+	/**
+	 * @param array $server
+	 * @param string $endpoint
+	 * @param array $params
+	 * @param string $method
+	 * @param bool $jsonResponse
+	 * @return array|mixed|resource|string|string[]
+	 * @throws PreConditionNotMetException
+	 */
+	public function request(array $server, string $endpoint, array $params = [], string $method = 'GET',
+		bool $jsonResponse = true) {
+		$verb = strtolower($method);
+		if (!in_array($verb, self::ALLOWED_VERBS)) {
+			return ['error' => $this->l10n->t('Bad HTTP method')];
+		}
+
+		try {
+			$url = $server['url'] . $endpoint;
+			$credentials = $server['api_key'];
+			$options = [
+				'headers' => [
+					'Authorization' => 'Bearer ' . $credentials,
+					'Content-Type' => 'application/scim+json; charset=utf-8',
+					'User-Agent' => Application::INTEGRATION_USER_AGENT,
+				],
+			];
+
+			if (count($params) > 0) {
+				if ($method === 'GET') {
+					$url .= '?' . http_build_query($params);
+				} else {
+					$options['json'] = $params;
+				}
+			}
+
+			$response = $this->client->{$verb}($url, $options);
+			if ($response->getStatusCode() >= 400) {
+				return ['error' => $this->l10n->t('Bad credentials')];
+			}
+
+			$body = $response->getBody();
+			return $jsonResponse ? json_decode($body, true) : $body;
+		} catch (ServerException|ClientException $e) {
+			$body = $e->getResponse()->getBody();
+			$this->logger->warning('SCIM API error : ' . $body, ['app' => Application::APP_ID]);
+			return ['error' => $e->getMessage()];
+		} catch (Exception|Throwable $e) {
+			$this->logger->warning('SCIM API error', ['exception' => $e, 'app' => Application::APP_ID]);
+			return ['error' => $e->getMessage()];
+		}
+	}
+}
---
+++ lib/Service/ScimApiService.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\ScimClient\Service;
+
+use OCA\ScimClient\AppInfo\Application;
+use OCP\Http\Client\IClient;
+use OCP\Http\Client\IClientService;
+use OCP\IL10N;
+use OCP\IURLGenerator;
+use OCP\PreConditionNotMetException;
+use OCP\Security\ICrypto;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Service to make requests to SCIM server
+ */
+class ScimApiService {
+
+	private IClient $client;
+
+	public function __construct(
+		IClientService $clientService,
+		private LoggerInterface $logger,
+		private IL10N $l10n,
+		private IURLGenerator $urlGenerator,
+		private ICrypto $crypto,
+		private NetworkService $networkService,
+	) {
+		$this->client = $clientService->newClient();
+	}
+
+	/**
+	 * @param array $server
+	 * @return array
+	 * @throws PreConditionNotMetException
+	 */
+	public function verifyScimServer(array $server): array {
+		$response = $this->networkService->request($server, '/ServiceProviderConfig', [], 'GET');
+
+		if (isset($response['error'])) {
+			$reponse['success'] = false;
+			return (array)$response;
+		}
+
+		$hasScimSchema = $response['schemas'][0] === Application::SCIM_CORE_SCHEMA . ':ServiceProviderConfig';
+		$hasBulkOperation = $response['bulk']['supported'];
+
+		if (!$hasScimSchema) {
+			return [
+				'error' => 'Unable to fetch SCIM config',
+				'success' => false,
+			];
+		}
+
+		if (!$hasBulkOperation) {
+			return [
+				'error' => 'Bulk operations feature is required',
+				'success' => false,
+			];
+		}
+
+		return ['success' => true];
+	}
+}
