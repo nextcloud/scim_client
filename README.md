306bf6ce014a8a65afc338f5105e89f3d71b6de8
Andy Scherzinger <info@andy-scherzinger.de>
2025-02-20 20:00:38 +0100

    docs(reuse): Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+<!--
+  - SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
 # SCIM Client
 
 Use Nextcloud as an identity provider for external services
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,11 @@
 
 declare(strict_types=1);
 
+/**
+ * SPDX-FileCopyrightText: 2024 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 require_once __DIR__ . '/../../../tests/bootstrap.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
