e1c87a7ece8140a1a30efdffde725c94747c541e
Julien Veyssier <julien-nc@posteo.net>
2025-12-29 18:00:57 +0100

    fix: update stylelint config
    Signed-off-by: Julien Veyssier <julien-nc@posteo.net>

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -5,4 +5,28 @@
 
 module.exports = {
 	extends: 'stylelint-config-recommended-vue',
+	rules: {
+		'rule-empty-line-before': [
+			'always',
+			{
+				ignore: ['after-comment', 'inside-block'],
+			},
+		],
+		'declaration-empty-line-before': [
+			'never',
+			{
+				ignore: ['after-declaration'],
+			},
+		],
+		'comment-empty-line-before': null,
+		'selector-type-case': null,
+		'no-descending-specificity': null,
+		'selector-pseudo-element-no-unknown': [
+			true,
+			{
+				ignorePseudoElements: ['v-deep'],
+			},
+		],
+	},
+	plugins: ['stylelint-scss'],
 }
